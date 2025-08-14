eb4c3ec52a2514d761b6397f9e269ee620beeea8
ExamFlowBot <examflow@example.com>
2025-08-14 13:30:34 +0000

    Add integrated bot management system

---
+++ bot/middleware.py
@@ -0,0 +1,14 @@
+from django.utils.deprecation import MiddlewareMixin
+from django.http import HttpResponse
+from .tasks import start_bot
+
+class AutoStartBotMiddleware(MiddlewareMixin):
+    """Middleware для автоматического запуска Telegram-бота"""
+    
+    def process_request(self, request):
+        # Запускаем бота при первом запросе к сайту
+        start_bot()
+        return None
+
+    def process_response(self, request, response):
+        return response
--- bot/views.py
+++ bot/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .tasks import get_bot_status
 
-# Create your views here.
+def bot_control_panel(request):
+    """Панель управления ботом"""
+    status = get_bot_status()
+    
+    return render(request, 'bot/panel.html', {
+        'status': status['status'],
+        'pid': status.get('pid', 'N/A')
+    })
+
+def bot_api_status(request):
+    """API для получения статуса бота"""
+    status = get_bot_status()
+    return JsonResponse(status)
