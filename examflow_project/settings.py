9f8f646fee547b5bcf8f43d5ad7559676496ed76
gauss <79516625803@ya.ru>
2025-08-18 10:38:18 +0300

    Update settings.py

--- examflow_project/settings.py
+++ examflow_project/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = 'django-insecure-b=8q90=se^7twm97htmj$v2n-(b@8!0(%h48n=tnb=t^%ja!ta
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['examflowbot1.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [h.strip() for h in os.getenv('ALLOWED_HOSTS', 'examflow.ru,www.examflow.ru,localhost,127.0.0.1').split(',') if h.strip()]
 
 
 # Application definition
@@ -137,3 +137,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
