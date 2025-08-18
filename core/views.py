5b41794dc790b8414d1851f3dd27b1e2611a0696
Vyacheslav <vyacheslav@example.com>
2025-08-18 12:56:14 +0300

    feat(ui): connect Bootstrap + static css/js, show subjects/tasks on home; add DEBUG static in urls

--- core/views.py
+++ core/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from .utils import generate_qr_code
+from .models import Subject, Task
 
 def home(request):
     qr_code = generate_qr_code("https://t.me/ExamFlowBot")
-    return render(request, 'home.html', {'qr_code': qr_code})
+    subjects = Subject.objects.all()[:6]
+    total_subjects = Subject.objects.count()
+    total_tasks = Task.objects.count()
+    context = {
+        'qr_code': qr_code,
+        'subjects': subjects,
+        'total_subjects': total_subjects,
+        'total_tasks': total_tasks,
+    }
+    return render(request, 'home.html', context)
--- examflow_project/urls.py
+++ examflow_project/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from core.views import home
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
