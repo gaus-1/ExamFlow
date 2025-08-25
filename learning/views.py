29cf9cf3b97529d9e131f93a6a6937b527ce006c
Vyacheslav <vyacheslav@example.com>
2025-08-25 10:09:52 +0300

    Исправляем ошибку 500: убираем неправильные запросы с topics в learning views

--- learning/views.py
+++ learning/views.py
@@ -36,10 +36,8 @@ def home(request):
     subjects_count = Subject.objects.count()  # type: ignore
     tasks_count = Task.objects.count()  # type: ignore
     
-    # Получаем предметы для отображения
-    subjects = Subject.objects.annotate(  # type: ignore
-        tasks_count=Count('topics__tasks')
-    ).filter(tasks_count__gt=0)[:8]  # Показываем первые 8 предметов
+    # Получаем предметы для отображения (без сложных запросов)
+    subjects = Subject.objects.all()[:8] # type: ignore
     
     context = {
         'subjects_count': subjects_count,
@@ -59,17 +57,16 @@ def subjects_list(request):
     - Количество тем и заданий
     - Прогресс пользователя (если авторизован)
     """
-    subjects = Subject.objects.annotate(  # type: ignore
-        topics_count=Count('topics'),
-        tasks_count=Count('topics__tasks')
-    ).filter(tasks_count__gt=0)
+    # Получаем все предметы (без сложных запросов)
+    subjects = Subject.objects.all()  # type: ignore
     
     # Добавляем прогресс для авторизованных пользователей
     if request.user.is_authenticated:
         for subject in subjects:
+            # Считаем решенные задания для предмета
             solved_tasks = UserProgress.objects.filter(  # type: ignore
                 user=request.user,
-                task__topic__subject=subject,
+                task__subject=subject,
                 is_correct=True
             ).count()
             subject.user_progress = solved_tasks
@@ -89,17 +86,16 @@ def subject_detail(request, subject_id):
     - Статистику решенных заданий
     """
     subject = get_object_or_404(Subject, id=subject_id)
-    topics = Topic.objects.filter(subject=subject).annotate(  # type: ignore
-        tasks_count=Count('tasks')
-    ).filter(tasks_count__gt=0)
+    # Получаем темы для предмета
+    topics = Topic.objects.filter(subject=subject)  # type: ignore
     
     # Статистика для авторизованных пользователей
     user_stats = {}
     if request.user.is_authenticated:
-        total_tasks = Task.objects.filter(topic__subject=subject).count()  # type: ignore
+        total_tasks = Task.objects.filter(subject=subject).count()  # type: ignore
         solved_tasks = UserProgress.objects.filter(  # type: ignore
             user=request.user,
-            task__topic__subject=subject,
+            task__subject=subject,
             is_correct=True
         ).count()
         
